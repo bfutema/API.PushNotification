f38a2600c7d5e6ab306797439dc7312baf638e0e
Bruno Futema <bruno.futema@outlook.com>
2020-01-29 15:02:35 -0300

    chore: instalando o cors

---
+++ src/API.PushNotification/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API.PushNotification.Controllers
+{
+    [System.Web.Http.RoutePrefix("api")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UserController : ApiController
+    {
+        
+	}
+}
